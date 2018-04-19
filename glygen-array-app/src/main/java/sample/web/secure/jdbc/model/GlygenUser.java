18cc8caebde38bc9f75da6a911924a526e89a863
sena <sena@192.168.168.62>
2018-04-19 14:44:07 -0400

    connects to postgres and uses a more detailed user object

---
+++ glygen-array-app/src/main/java/sample/web/secure/jdbc/model/GlygenUser.java
@@ -0,0 +1,58 @@
+package sample.web.secure.jdbc.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class GlygenUser extends User {
+
+	private static final long serialVersionUID = -3531439484732724601L;
+
+    private final String firstName;
+    private final String lastName;
+    private final String affiliation;
+    private final String affiliationWebsite;
+    private final Boolean publicFlag;
+
+    public GlygenUser(String username, String password, boolean enabled,
+        boolean accountNonExpired, boolean credentialsNonExpired,
+        boolean accountNonLocked,
+        Collection<? extends GrantedAuthority> authorities,
+        String firstName, String lastName, String affiliation,
+        String affiliationWebsite, boolean publicFlag) {
+
+            super(username, password, enabled, accountNonExpired,
+               credentialsNonExpired, accountNonLocked, authorities);
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.affiliation = affiliation;
+            this.affiliationWebsite = affiliationWebsite;
+            this.publicFlag = publicFlag;
+    }
+
+    public static long getSerialversionuid() {
+       return serialVersionUID;
+    }
+
+    public String getFirstName() {
+       return firstName;
+    }
+
+    public String getLastName() {
+       return lastName;
+    }
+    
+    public String getAffiliation() {
+		return affiliation;
+	}
+    
+    public String getAffiliationWebsite() {
+		return affiliationWebsite;
+	}
+    
+    public Boolean getPublicFlag() {
+		return publicFlag;
+	}
+}
--- glygen-array-app/src/main/resources/schema.sql
+++ glygen-array-app/src/main/resources/schema.sql
@@ -1,7 +1,15 @@
+drop table users;
+drop table authorities;
+
 create table users (
   username varchar(256),
   password varchar(256),
-  enabled boolean
+  enabled boolean,
+  firstname varchar(256),
+  lastname varchar(256),
+  affiliation varchar(256),
+  affiliationWebsite varchar(256),
+  publicflag boolean
 );
 
 create table authorities (
