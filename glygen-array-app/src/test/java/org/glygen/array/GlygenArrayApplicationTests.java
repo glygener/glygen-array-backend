2a4aa0471c757aff9b9a45f4e5894953b3ff4aa8
vinamrjain <er.vinamrajain@gmail.com>
2018-04-27 15:43:05 -0400

    Logger added

--- glygen-array-app/src/test/java/org/glygen/array/GlygenArrayApplicationTests.java
+++ glygen-array-app/src/test/java/org/glygen/array/GlygenArrayApplicationTests.java
@@ -2,8 +2,12 @@ package org.glygen.array;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,13 @@ public class GlygenArrayApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testLogging() {
+		final Logger log = LoggerFactory.getLogger(org.glygen.array.SampleWebSecureJdbcApplication.class);
+		log.info("Info test");
+		log.debug("Debug test");
+		log.warn("Warning test");
+		
+	}
+	
 }
