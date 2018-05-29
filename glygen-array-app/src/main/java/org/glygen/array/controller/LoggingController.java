55c7d53752d464c726d5a1e66eb8ecddba056f36
vinamrjain <er.vinamrajain@gmail.com>
2018-05-29 14:48:50 -0400

    loggig controller added

---
+++ glygen-array-app/src/main/java/org/glygen/array/controller/LoggingController.java
@@ -0,0 +1,31 @@
+package org.glygen.array.controller;
+
+import org.glygen.array.GlygenArrayApplication;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/logger")
+public class LoggingController {
+
+	public static Logger logger=(Logger) LoggerFactory.getLogger(GlygenArrayApplication.class);
+	
+	@GetMapping("/logInfo")
+	public String logInfo() {
+		logger.info("");
+		return "Info logged";
+	}
+	
+	@GetMapping("/logError")
+	public String logError() {
+		logger.error("");
+		return "Error logger";
+	}
+	
+}
