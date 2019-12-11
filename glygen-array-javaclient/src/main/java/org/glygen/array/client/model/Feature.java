c1e0e9c65e1e890cc527606b654d6328ff479586
sena <sena.arpinar@gmail.com>
2019-12-11 12:44:21 -0500

    added feature type to handle controls

--- glygen-array-javaclient/src/main/java/org/glygen/array/client/model/Feature.java
+++ glygen-array-javaclient/src/main/java/org/glygen/array/client/model/Feature.java
@@ -1,18 +1,46 @@
 package org.glygen.array.client.model;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Feature {
+	String id;
 	String uri;
+	String name;
 	List<Glycan> glycans;
 	Linker linker;
 	Double ratio = null;   // in percentages like 100.0%, 50.0% etc.
 	
 	Map<Glycan, Integer> positionMap = new HashMap<>();
 	
+	FeatureType type = FeatureType.NORMAL;
 	
+	Date dateModified;
+	Date dateCreated;
+	Date dateAddedToLibrary;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+        return name;
+    }
+	
+	public void setName(String name) {
+        this.name = name;
+    }
+	
+	@JsonIgnore
 	public Map<Glycan, Integer> getPositionMap() {
 		return positionMap;
 	}
@@ -31,6 +59,12 @@ public class Feature {
 	 */
 	public void setGlycans(List<Glycan>glycan) {
 		this.glycans = glycan;
+	}
+	
+	public void addGlycan (Glycan glycan) {
+		if (this.glycans == null)
+			glycans = new ArrayList<Glycan>();
+		glycans.add(glycan);
 	}
 	/**
 	 * @return the linker
@@ -77,4 +111,61 @@ public class Feature {
 			return  uri.equals(((Feature)obj).getUri());
 		return super.equals(obj);
 	}
+
+	/**
+	 * @return the dateModified
+	 */
+	public Date getDateModified() {
+		return dateModified;
+	}
+
+	/**
+	 * @param dateModified the dateModified to set
+	 */
+	public void setDateModified(Date dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	/**
+	 * @return the dateCreated
+	 */
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	/**
+	 * @param dateCreated the dateCreated to set
+	 */
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	/**
+	 * @return the dateAddedToLibrary
+	 */
+	public Date getDateAddedToLibrary() {
+		return dateAddedToLibrary;
+	}
+
+	/**
+	 * @param dateAddedToLibrary the dateAddedToLibrary to set
+	 */
+	public void setDateAddedToLibrary(Date dateAddedToLibrary) {
+		this.dateAddedToLibrary = dateAddedToLibrary;
+	}
+
+	/**
+	 * @param positionMap the positionMap to set
+	 */
+	public void setPositionMap(Map<Glycan, Integer> positionMap) {
+		this.positionMap = positionMap;
+	}
+	
+	public void setType(FeatureType type) {
+        this.type = type;
+    }
+	
+	public FeatureType getType() {
+        return type;
+    }
 }
