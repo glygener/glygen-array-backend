33d2fa7f2fe31eeb503ccdef10f92e646e09f345
sena <sena.arpinar@gmail.com>
2020-06-26 19:14:04 -0400

    updated initialization to prevent public access to private graphs

--- virtuoso/initialize.sql
+++ virtuoso/initialize.sql
@@ -1,3 +1,4 @@
 RDF_DEFAULT_USER_PERMS_SET ('nobody', 0);
 RDF_GRAPH_USER_PERMS_SET ('http://glygen.org/glygenarray/public', 'nobody', 1);
 RDF_GRAPH_GROUP_CREATE ('http://glygen.org/glygenarray/private', 1);
+RDF_GRAPH_USER_PERMS_SET ('http://glygen.org/glygenarray/private', 'nobody', 0);
