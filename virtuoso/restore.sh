c20824a93ff7cd6819cbded1d7ea5a284e5c9aa6
sena <sena.arpinar@gmail.com>
2021-06-24 14:57:32 -0400

    changed the backup file prefix

--- virtuoso/restore.sh
+++ virtuoso/restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose down
-docker run --rm  -it -v $HOME/glygen-array/virtuoso:/data tenforce/virtuoso:1.3.1-virtuoso7.2.2 virtuoso-t +restore-backup backups/backup_1.bp +configfile /data/virtuoso.ini
-docker-compose up -d
+echo "executing restore"
+docker run --rm  -it -v $HOME/glygen-array/virtuoso:/data tenforce/virtuoso:1.3.1-virtuoso7.2.2 virtuoso-t +restore-backup backup_ +configfile /data/virtuoso.ini
+echo "done"
