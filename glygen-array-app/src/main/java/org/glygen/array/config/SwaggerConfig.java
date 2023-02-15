530c321f15caae1040d5f1aa8c57302452d9b860
sena <sena.arpinar@gmail.com>
2023-02-15 14:42:04 -0500

    trying to fix swagger on the server

--- glygen-array-app/src/main/java/org/glygen/array/config/SwaggerConfig.java
+++ glygen-array-app/src/main/java/org/glygen/array/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package org.glygen.array.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,14 +8,26 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {    
+    
+    @Value("${glygen.host}")
+    private String host;
+    
+    @Value("${glygen.basePath}")
+    private String basePath;
+    
+    @Value("${glygen.scheme}")
+    private String scheme;
+    
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Glygen Array Repository")
                 .description("Glygen Array Repository Web Services"))
+                .addServersItem(new Server().url(scheme + host + basePath))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                         new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
