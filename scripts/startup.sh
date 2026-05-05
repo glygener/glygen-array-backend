fbefa24b8f3f2cf574e79844c5846ca0c502fdc4
Sena <sena@Senas-iMac.local>
2026-05-05 15:15:02 -0400

    updated docker version

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,12 +2,12 @@
 # set environment variables
 . ~/workspace/glygen-array-backend/bashrc
 # start apache
-cd ~/workspace/glygen-array-backend/apache && docker-compose up -d
+cd ~/workspace/glygen-array-backend/apache && docker compose up -d
 # start postgres
-cd ~/workspace/glygen-array-backend/postgres && docker-compose up -d
+cd ~/workspace/glygen-array-backend/postgres && docker compose up -d
 # start virtuoso
-cd ~/workspace/glygen-array-backend/virtuoso && docker-compose up -d
+cd ~/workspace/glygen-array-backend/virtuoso && docker compose up -d
 # start backend application
-cd ~/workspace/glygen-array-backend/glygen-array-app && docker-compose up -d
+cd ~/workspace/glygen-array-backend/glygen-array-app && docker compose up -d
 # start frontend application
-cd ~/workspace/glygen-array-frontend && docker-compose up -d --build && docker images -qf dangling=true | xargs docker rmi
+cd ~/workspace/glygen-array-frontend && docker compose up -d --build && docker images -qf dangling=true | xargs docker rmi
